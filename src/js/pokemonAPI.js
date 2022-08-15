c807774d9c6d7165231f651a558e3f374ea5e61c
Rhuan Lucas <rhuanfernandes.rf@gmail.com>
2022-08-15 12:11:15 -0300

    Corrigindo erro de inserção de habilidades

--- src/js/pokemonAPI.js
+++ src/js/pokemonAPI.js
@@ -1,11 +1,8 @@
 const ul = document.querySelector("main ul");
-const number = 10;
+const number = 100;
 const baseUrl = `https://pokeapi.co/api/v2/pokemon?limit=${number}&offset=0`;
 var pokemon;
 var count = 1;
-// const name = document.querySelector('main ul li .informacoes h2');
-// const hp = document.querySelector('main ul li .informacoes p');
-// const imagePokemon = document.querySelector('main ul li .containr-img img')
 
 connect();
 function connect(){
@@ -45,12 +42,17 @@ function insertCard(pokemon){
   var hp = pokemon.base_experience;
   var imagePokemon = pokemon.sprites.other.home.front_default;
   const id = pokemon.id;
-  var ability1 = pokemon.abilities[0].ability.name;
-  var ability2 = pokemon.abilities[1].ability.name;
 
+  if(pokemon.abilities[0] !== undefined){
+    var ability1 = pokemon.abilities[0].ability.name;
+    ability1 = ability1[0].toUpperCase() + ability1.substr(1);
+  }
+  if(pokemon.abilities[1] !== undefined){
+    var ability2 = pokemon.abilities[1].ability.name;
+    ability2 = ability2[0].toUpperCase() + ability2.substr(1);
+  }
+  
   name = name[0].toUpperCase() + name.substr(1);
-  ability1 = ability1[0].toUpperCase() + ability1.substr(1);
-  ability2 = ability2[0].toUpperCase() + ability2.substr(1);
 
   const html = `
       <li class="fundo">
